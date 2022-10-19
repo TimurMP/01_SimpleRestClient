5ce075fd3b3203e21d7678b6b9e6355fc9ce7a55
TimurMP <titimurmamyshev@gmail.commur>
2022-10-19 15:34:06 +0300

    Final

--- src/main/java/telran/post/controller/PostClientAppl.java
+++ src/main/java/telran/post/controller/PostClientAppl.java
@@ -1,2 +1,22 @@
-package telran.post.controller;public class PostClientAppl {
+package telran.post.controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class PostClientAppl {
+
+    public static void main(String[] args) throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<String> request = new RequestEntity<>(HttpMethod.GET,new URI("https://jsonplaceholder.typicode.com/posts/8"));
+        ResponseEntity<String>  response =  restTemplate.exchange(request, String.class );
+
+        System.out.println(response.getStatusCodeValue());
+        System.out.println(response.getBody());
+        System.out.println(response.getHeaders());
+    }
 }
